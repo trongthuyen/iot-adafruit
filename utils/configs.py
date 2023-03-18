9d4d5c57df62b93498bf22d932edce239117d016
Trong Thuyen <trongthuyen2606@gmail.com>
2023-03-18 17:49:50 +0700

    Init IoT project app

--- utils/configs.py
+++ utils/configs.py
@@ -1,6 +1,6 @@
 from Adafruit_IO import MQTTClient
 ADAFRUIT_IO_USERNAME = "trongthuyen"
-ADAFRUIT_IO_KEY = "aio_TZtf11YRmxGMqMF1ncSwWh1p3WPt"
+ADAFRUIT_IO_KEY = "aio_RqwR50i6BiFHXq19jl8mQrFwnCKI"
 
 FEED_TEMP = "bbc-temp"
 FEED_INTENSITY = "bbc-intensity"
